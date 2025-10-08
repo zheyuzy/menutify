34193c8de9470d02853c6ad3c94b9bb32ee6eb71
zheyuzy <zheyuzheyee@gmail.com>
2025-10-08 12:20:08 +0800

    Feat: Add 'chef/chefs' label to genre count display

--- src/App.js
+++ src/App.js
@@ -197,7 +197,7 @@ function App() {
                           <span className="item-index">{index + 1}.</span>
                           <span className="item-name">{item.name}</span>
                           {dataType === 'tracks' && item.artists && <span className="item-artist">{item.artists.map(a => a.name).join(', ')}</span>}
-                          {dataType === 'genres' && item.count && <span className="item-artist">({item.count})</span>}
+                          {dataType === 'genres' && item.count && <span className="item-artist">({item.count} {item.count === 1 ? 'chef' : 'chefs'})</span>}
                         </div>
                       </div>
                     ))}
